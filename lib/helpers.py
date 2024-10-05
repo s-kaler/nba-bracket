4f733b43dd8a182213aa04502c4b4111c7a15eed
Sundeep <sundeep.kaler@gmail.com>
2024-10-05 15:21:10 -0400

    fixed party conditions

--- lib/helpers.py
+++ lib/helpers.py
@@ -154,23 +154,29 @@ def remove_pokemon_from_party():
 
 
 def add_pokemon_to_party():
+    all_team = Team.get_all()
     party = Team.find_by_party()
+    
     if len(party) == 6:
         print("You cannot add more than 6 members to the party.")
         return
     else:
-        print("Which party member do you want to add?")
-        nickname = input("> ")
-        new_party_member = Team.find_by_nickname(nickname)
-        if new_party_member:
-            if new_party_member.in_party == 0:
-                new_party_member.in_party = 1
-                new_party_member.update()
-                print(f'Success: {nickname} has been removed from the party.')
-            else:
-                print(f'{nickname} is already in the party.')
+        if len(party) == len(all_team):
+            print("You do not have any pokemon to add to the party")
+            return
         else:
-            print(f'Party member {nickname} not found.')
+            print("Which party member do you want to add?")
+            nickname = input("> ")
+            new_party_member = Team.find_by_nickname(nickname)
+            if new_party_member:
+                if new_party_member.in_party == 0:
+                    new_party_member.in_party = 1
+                    new_party_member.update()
+                    print(f'Success: {nickname} has been removed from the party.')
+                else:
+                    print(f'{nickname} is already in the party.')
+            else:
+                print(f'Party member {nickname} not found.')
 
 
 #for catching pokemon, if the wild pokemon is between certain levels, it will be harder to catch
@@ -241,13 +247,13 @@ def release_team():
     team_all = Team.get_all()
     if len(team_all) == 1:
         print("You cannot release the last pokemon from the team!")
-        return
+        return 0
     print("Choose a pokemon to release:")
     nickname = input("> ")
     team_member = Team.find_by_nickname(nickname)
-    released_money = Pokemon.find_by_id(team_member.pokemon_id).level * 3
     
     if team_member:
+        released_money = Pokemon.find_by_id(team_member.pokemon_id).level * 3
         if team_member.id == 1:
             print("You cannot release your starter pokemon from the team!")
             return 0
