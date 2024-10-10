5a7ed7a44e0e75e8de94ae4548b2e1a0a5a2dcf7
Sundeep <sundeep.kaler@gmail.com>
2024-10-10 01:20:49 -0400

    changed menu function in cli to be cohesive

--- lib/cli.py
+++ lib/cli.py
@@ -11,21 +11,23 @@ def main():
     greeting()
     while True:
         menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            teams_menu()
-        elif choice == "2":
-            players_menu()
+        
 
 
 def menu():
+    print("")
     print("Main Menu")
     print(" 0. Exit")
     print(" 1. Team Management")
     print(" 2. Player Management")
-
+    choice = input("> ")
+    if choice == "0":
+        exit_program()
+    elif choice == "1":
+        teams_menu()
+    elif choice == "2":
+        players_menu()
+        
 if __name__ == "__main__":
     main()
 
